ec391769799aafdb2b66620b91f64a4e2622ee44
Nguyen Quoc Tuan <quoctuan21698@gmail.com>
2023-02-16 11:25:46 +0700

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-100days-news';
+  name = 'angular' + VERSION.major;
+  user = {
+    name: 'tuấn',
+    age: 25,
+  };
+  handle() {
+    console.log('Click me');
+  }
 }
